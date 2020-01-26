fc2a5055a7307ef6ea088b8b3948b27c9b528369
Alberth Matos <alberth@matos.cc>
2020-01-26 14:02:54 -0500

    Updated bootstrap script with new name

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,18 @@ xcode-select --install
 
 mkdir ~/temp
 cd ~/temp
+
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
 python get-pip.py --user
+
 CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments ~/Library/Python/2.7/bin/pip install --user ansible
+
 rm get-pip.py
-git clone https://github.com/amatos/mac-dev-playbook
-cd mac-dev-playbook
+
+git clone https://github.com/amatos/macos-playbook
+
+cd macos-playbook
+
 ansible-galaxy install -r requirements.yml
 ansible-playbook main.yml -i inventory -K
